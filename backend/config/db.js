d7d66c025fc1f7ace4178ca515fc95602c640a3f
Naga Pradeepa <jsp695960@gmail.com>
2025-10-23 17:28:48 +0530

    Fix DB connection to use environment variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     
-    const mongoUri = 'mongodb+srv://jsp695960_db_user:2233445566@cluster0.bjysnf1.mongodb.net/food-delivery-db?retryWrites=true&w=majority';
+    const mongoUri = 'mongodb+srv://jsp695960_db_user:NEWQUdGdaLP%216Gp@cluster0.bjysnf1.mongodb.net/food-delivery-db?retryWrites=true&w=majority';
 
     try {
         await mongoose.connect(mongoUri);
